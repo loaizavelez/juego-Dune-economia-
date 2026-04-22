b4de3aa255d16e78628e0b27c5e2ef7687a0a7d5
loaizavelez <miguel.loaizav@upb.edu.co>
2026-04-22 12:47:53 -0500

    click del jugador
    el jugador ya puede capturar casillas

--- Assets/Scripts/Tile/Casillas.cs
+++ Assets/Scripts/Tile/Casillas.cs
@@ -20,7 +20,24 @@ public class Casillas : MonoBehaviour
         // Inicializa el color según el estado del Inspector
         SetController(controller);
     }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
+            if (hit.collider != null)
+            {
+                Casillas c = hit.collider.GetComponent<Casillas>();
+                if (c != null)
+                {
+                    Debug.Log($"Click detectado en casilla ({c.x}, {c.y})");
+                    c.SetController(Controller.Player);
+                }
+            }
+        }
+    }
     public void SetController(Controller newController)
     {
         controller = newController;
@@ -47,8 +64,9 @@ public class Casillas : MonoBehaviour
         Debug.Log($"Casilla ({x}, {y}) ahora pertenece a {controller} y cambió a color {newColor}");
     }
 
-    void OnMouseDown()
+   /* void OnMouseDown()
     {
+        Debug.Log($"Casilla ({x}, {y}) fue clickeada por el jugador");
         SetController(Controller.Player);
-    }
+    }*/
 }
